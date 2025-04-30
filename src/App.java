9c289c80ae11d8ea3ebe17760fa58a6df67e5dfe
Sebas <2006sacura@gmail.com>
2025-04-29 19:04:07 -0500

    Se termino todo

--- src/App.java
+++ src/App.java
@@ -77,6 +77,10 @@ public class App {
         System.out.println(
                 "Resultado selección Metodo 1: " +
                         java.util.Arrays.toString(arregloInsercionOrdenado3));
+                        
 
     }
 }
+
+
+//SeBASTIAN lOPEZ
